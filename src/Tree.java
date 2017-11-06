1da71237965e204770673b217a9db77e91a4c886
igriffin415 <fireice16@yahoo.com>
2017-11-06 15:55:58 -0500

    Tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,35 @@
+import processing.core.PApplet;
+
 //ten branches, call draw for each branch
 public class Tree {
+	PApplet app;
+	float start;
+	Branch[] branches;
 
+	public Tree(PApplet a) {
+		app = a;
+		branches = new Branch[10];
+		fillBranches();
+	}
+	
+	
+	/* fills the branch array */
+	private void fillBranches() {
+		for(Branch branch : branches) {
+			//create a branch 
+			//put branch in array
+		}
+	}
+	
+	public void update() {
+		for(Branch branch : branches) {
+			//update each branch
+		}
+	}
+	
+	public void draw() {
+		for(Branch branch : branches) {
+			//draw each branch
+		}
+	}
 }
