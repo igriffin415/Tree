8c3d5a3fbb0fb7f00908618d19a77a3a3a77e7cf
Veneta Cholakova <venetacholakova@gmail.com>
2017-11-06 19:07:59 -0500

    Branch
    Branch stuff added /not finished/

--- src/Branch.java
+++ src/Branch.java
@@ -1,5 +1,43 @@
+import java.awt.Color;
+import processing.core.PApplet;
+
 public class Branch {
 	// starting point (x,y)
 	// drawing a curve
 	// every (TIME) change random angle
+
+	private static final float WIDTH = .2f;
+	private static final float DECREASE = .005f;
+
+	float xPos;
+	float yPos;
+	float angle;
+	
+	private PApplet applet;
+	boolean popped;
+
+
+	public Branch(float x, float y, float angle, PApplet a){
+		this.xPos = x;
+		this.yPos = y;
+		this.angle = angle;
+		this.applet = a;
+				
+		draw();
+	}
+
+	public void draw(){
+		applet.noFill();
+		applet.stroke(255, 255, 0);
+		
+		applet.beginShape();
+
+		applet.line(85, 20, 10, 10);
+		applet.line(90, 90, 15, 80);
+		applet.stroke(0, 0, 0);
+		applet.bezier(85, 20, 10, 10, 90, 90, 15, 80);
+		
+		applet.endShape();
+		
+	}	
 }
