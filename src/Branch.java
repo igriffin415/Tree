4dce4352e9a9657fafa45e1213c7214089382388
Veneta Cholakova <venetacholakova@gmail.com>
2017-11-08 15:24:22 -0500

    branch alternative
    alternative curves

--- src/Branch.java
+++ src/Branch.java
@@ -37,7 +37,18 @@ public class Branch {
 		
 		applet.noFill();
 		applet.stroke(255, 255, 0);
-		applet.line(xPos, yPos, xEnd, yEnd);	
+		applet.line(xPos, yPos, xEnd, yEnd);
+		
+		
+		/*applet.beginShape();
+		// midpoint
+		float midX = (xPos + xEnd) /2;
+		float midY = (yPos + yEnd) /2;
+		//curve 1
+		applet.curve(xPos, yPos, 3.5f*(xPos + midX), .5f*(xPos + midX), 5.5f*(xPos + midX), 10.5f*(xPos + midX), midX, midY);
+		//curve 2
+		applet.curve(midX, midY, 3.5f*(xPos + midX), 3.5f*(xPos + midX), 50.0f, 20.0f, xEnd, yEnd);
+		applet.endShape(); */
 		
 	}	
 }
