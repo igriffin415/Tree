85b4ec5e84a7827702ce90fbc6906ae9657c0ac3
kim53s <kim53s@mtholyoke.edu>
2017-11-10 11:37:41 -0500

    random speed&size / fadeaway method
    - each leaf is different in size
    - it falls at random speed
    - implemented fadeaway method so that leaves start fade away when they touched the ground

--- src/Leaf.java
+++ src/Leaf.java
@@ -5,21 +5,25 @@ public class Leaf {
 	
 	PApplet app;
 	float x, y;
-	
+	float size;
 	// falling speed
 	float speed;
 	// random greenish color
 	int[] color = new int[3];
+	int transparency = 170;
 	int x_direction = (int)(Math.random()*2);
 	int changeInGreen = 0;
 	static final int MAX_RED = 220; 
 	static final int MAX_GREEN = (int)(Math.random()*255); 
 	
 	public Leaf(PApplet a, float x, float y){
-		app = a;
+		this.app = a;
 		this.x = x;
 		this.y = y;
-		speed = .004f;
+		// random falling speed
+		speed = (float)Math.random()*0.003f + 0.002f;
+		// random size of leaf
+		this.size = (float)Math.random()*0.08f + 0.13f;
 		// generate random greenish color
 		color[0] = (int)(Math.random()*80);
 		color[1] = (int)(Math.random()*90)+140;
@@ -35,21 +39,24 @@ public class Leaf {
 			else
 				x = x + 0.002f;
 		}
+		else
+			fadeaway();
 	}
 	
+	// leaf fade away
+	public void fadeaway(){
+		if(transparency > 0)
+			transparency= transparency-5;
+	}	
 	// return true if the leaf touched the ground
 	public boolean touched(){
 		return y <= -1f;
 	}
 	
-	public void blossom(){
-		
-	}
-	
 	public void turnRed(){
 		if(color[0] < MAX_RED )
 			color[0] = color[0] + 1;
-		if(changeInGreen < 50){
+		if(changeInGreen < 60){
 			color[1] = color[1] - 1;
 			changeInGreen++;
 		}
@@ -57,8 +64,7 @@ public class Leaf {
 	}
 	
 	public void draw(){
-		app.fill(color[0], color[1], color[2], 170);
-		app.ellipse(x, y, .2f, .2f);
+		app.fill(color[0], color[1], color[2], transparency);
+		app.ellipse(x, y, size, size);
 	}
-	
 }
